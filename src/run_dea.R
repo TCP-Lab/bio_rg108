a07b0cfe57c915e7981efc5f8b94c4e9a2ef683f
Luca Visentin <luca.visentin@unito.it>
2024-09-12 14:31:13 +0200

    update dea

--- src/run_dea.R
+++ src/run_dea.R
@@ -1,11 +1,15 @@
 
 library(tidyverse)
+requireNamespace("DeNumerator")
 requireNamespace("DESeq2")
 
 sanitize_names <- function(x) {
     x %>% str_replace_all(" ", "_")
 }
 
+#' Load and clean the input data
+#' 
+#' Paths are default for this project
 load_data <- function(
         data_path = "./data/bioRG108_CountMatrix_genes_expected_count.tsv",
         metadata_path = "./data/in/Checklist_ENA-default sample checklist_1721814613698.tsv"
@@ -35,24 +39,40 @@ load_data <- function(
     )
 }
 
-main <- function() {
-    data <- load_data()
-    
-    # Check the colnames/rownames order since DeSeq2 is dumb
-    data$counts <- data$counts[, rownames(data$metadata$sample)]
-    assertthat::are_equal(colnames(data$counts), rownames(data$metadata$sample))
-    
-    # We round the data when loading the matrix since Deseq doesn't like the
-    # RSEM partial abundances
-    
-    dds <- DESeq2::DESeqDataSetFromMatrix(
-        round(data$counts),
-        data$metadata$sample,
-        ~ cell_type + media + cell_type:media
-    )
-    
-    # We have liftoff
-    dds_res <- DESeq2::DESeq(dds)
-}
+data <- load_data()
 
-main()
+# Check the colnames/rownames order since DeSeq2 is dumb
+data$counts <- data$counts[, rownames(data$metadata$sample)]
+assertthat::are_equal(colnames(data$counts), rownames(data$metadata$sample))
+
+# We round the data when loading the matrix since Deseq doesn't like the
+# RSEM partial abundances
+
+dds <- DESeq2::DESeqDataSetFromMatrix(
+    round(data$counts),
+    data$metadata$sample,
+    ~ cell_type + media + cell_type:media
+)
+
+# We have liftoff
+dds_res <- DESeq2::DESeq(dds)
+
+# Routine plots
+dds_res %>% DESeq2::plotMA()
+# Looks good to me!
+
+# Enumerate the results
+enum <- DeNumerator::denumerate(
+    dds_res, alpha=0.05, fold_change=1,
+    new_labels=list(
+        "cell_type_Periodontal_ligament_stem_cells_vs_Dental_pulp_stem_cells" = "cell_type:ligament",
+        "media_mcdb131_vs_amem" = "media:mcdb131",
+        "cell_typePeriodontal_ligament_stem_cells.mediamcdb131" = "int:media_on_ligament"
+    )
+)
+
+DeNumerator::plot_enumeration_frame(
+    enum,
+    title = "Denumeration of Biorg_108",
+    exclude_all_negative = TRUE
+)
